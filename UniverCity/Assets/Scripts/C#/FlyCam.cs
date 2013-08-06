9591916b88ef8a1539d9435a5b1ee321e58d7e14
shadyendless <dalindeck@gmail.com>
2013-08-06 14:05:20 -0700

    Revert "Fixed the height bounds for Explorer."
    This reverts commit 100eed82daeec933ad78d00d3ad6f49ec968849a.

--- UniverCity/Assets/Scripts/C#/FlyCam.cs
+++ UniverCity/Assets/Scripts/C#/FlyCam.cs
@@ -39,7 +39,6 @@ public class FlyCam : MonoBehaviour
 	
 	void Update()
 	{
-		/*
         if (Application.loadedLevel != 2)
         {
             Screen.lockCursor = true;
@@ -105,16 +104,6 @@ public class FlyCam : MonoBehaviour
         transform.position = new Vector3(transform.position.x,
                                          Mathf.Clamp(f, minHeight, maxHeight),
                                          transform.position.z);
-		*/
-
-		if (transform.position.y < minHeight)
-			transform.position = new Vector3 (transform.position.x,
-			                                 minHeight,
-			                                 transform.position.z);
-		else if (transform.position.y > maxHeight)
-			transform.position = new Vector3 (transform.position.x,
-			                                  maxHeight,
-			                                  transform.position.z);
 	}
 	
 	Vector3 GetBaseInput()
