af79a6edb4314db8a1d71d9c95d76f7f8bddb6e3
shadyendless <dalindeck@gmail.com>
2013-08-06 14:09:49 -0700

    Fixed the height again.
    Stupid detached head nonsense made this take way longer.

--- UniverCity/Assets/Scripts/C#/FlyCam.cs
+++ UniverCity/Assets/Scripts/C#/FlyCam.cs
@@ -39,6 +39,7 @@ public class FlyCam : MonoBehaviour
 	
 	void Update()
 	{
+        /*
         if (Application.loadedLevel != 2)
         {
             Screen.lockCursor = true;
@@ -101,8 +102,9 @@ public class FlyCam : MonoBehaviour
 		p *= Time.deltaTime;
 		f = p.y + transform.position.y;
 		transform.Translate(p);
+        */
         transform.position = new Vector3(transform.position.x,
-                                         Mathf.Clamp(f, minHeight, maxHeight),
+                                         Mathf.Clamp(transform.position.y, minHeight, maxHeight),
                                          transform.position.z);
 	}
 	
