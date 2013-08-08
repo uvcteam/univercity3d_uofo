de9317f635a7995caac038a1de6890f80cb51d7a
Alex Ford <alex.ford1337@gmail.com>
2013-08-07 19:28:13 -0700

    Union Hall home screen UI overhual
    Looks awesome

---
+++ UniverCity/Assets/Scripts/BottomMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class BottomMenu : MonoBehaviour 
+{
+    public GameObject[] buttons = new GameObject[5];
+    int activeScene = 4;
+
+    void Start()
+    {
+        //foreach(GameObject button in buttons)
+        //    NGUITools.AddWidgetCollider(button);
+        activeScene = Application.loadedLevel - 1;
+        if (activeScene != -1)
+            buttons[activeScene].SendMessage("DeActivate");
+    }
+
+    void OnArcadeClicked()
+    {
+        Application.LoadLevel(2);
+        
+    }
+
+    void OnExplorerClicked()
+    {
+        Application.LoadLevel(1);
+    }
+
+    void OnMemoryBankClicked()
+    {
+        Application.LoadLevel(4);
+    }
+
+    void OnUnionHallClicked()
+    {
+        Application.LoadLevel(3);
+    }
+
+    void OnVirtualMallClicked()
+    {
+        Application.LoadLevel(5);
+    }
+}
