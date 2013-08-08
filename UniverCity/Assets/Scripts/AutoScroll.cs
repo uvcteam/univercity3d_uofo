d8b88d923defb034040b730b48cf5212460ec155
Alex Ford <alex.ford1337@gmail.com>
2013-08-08 13:42:34 -0700

    Virtual Mall UI

---
+++ UniverCity/Assets/Scripts/AutoScroll.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class AutoScroll : MonoBehaviour 
+{
+    private bool _pressed = false;
+    private float _lastTimeTouched = 0.0f;
+
+    public Vector3 start = new Vector3(500.0f, -7.5f, 0.0f);
+    public float waitTime = 5.0f;
+	
+	// Update is called once per frame
+	void LateUpdate () 
+    {
+        if (transform.position.y > 6.5f)
+        {
+            transform.localPosition = start;
+            Debug.Log("Wrap back");
+        }
+
+        if ((Time.time - _lastTimeTouched > waitTime) && !_pressed)
+        {
+            Debug.Log("Go");
+            transform.Translate(new Vector3(0.0f, 0.25f * Time.deltaTime, 0.0f));
+        }
+	}
+
+    void OnPressed()
+    {
+        _pressed = true;
+    }
+
+    void OnReleased()
+    {
+        _lastTimeTouched = Time.time;
+        _pressed = false;
+        Debug.Log("Released");
+    }
+}
