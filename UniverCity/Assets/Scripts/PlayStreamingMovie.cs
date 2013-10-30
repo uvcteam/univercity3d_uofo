83ace61d1fba245ec97b85859db0457429d2cca6
Jacob Foster <dalindeck@gmail.com>
2013-10-29 19:41:36 -0700

    Login menu fix and video loading fix.

--- UniverCity/Assets/Scripts/PlayStreamingMovie.cs
+++ UniverCity/Assets/Scripts/PlayStreamingMovie.cs
@@ -86,6 +86,7 @@ public class PlayStreamingMovie : PlayHardwareMovieClassPro {
 				MakeTextures(textureWidth,textureHeight);
 				print("Textures " + textureWidth + " " + textureHeight);
 				setTiling(1.0f);
+			    NativeDialogs.Instance.HideProgressDialog();
 			}
 		}
 		if(streamEarly) {
@@ -107,7 +108,6 @@ public class PlayStreamingMovie : PlayHardwareMovieClassPro {
 			streamEarly=true;
 		} else {
 			playMovie = true; // only play once stream is ready.
-            NativeDialogs.Instance.HideProgressDialog();
 		}
 	}
 	
