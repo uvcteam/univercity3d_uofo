323e66679936180cdfe353bb9d161fb1280b6a07
Alex Ford <alex.ford1337@gmail.com>
2013-11-12 18:21:12 -0800

    Fixed pages to work on Android -Moved script includes inside body tag

--- UniverCity/HTML_UI/VirtualMall/js/infinitescroll.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
-        $(window).scrollTop(1);
-    }
-    else if ($(window).scrollTop() == 0) {
-        $(window).scrollTop($(document).height() - $(window).height() - 1);
-    }
-});
