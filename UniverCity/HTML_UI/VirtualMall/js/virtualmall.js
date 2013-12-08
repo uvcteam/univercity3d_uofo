590d0100623869d4a1b5a794a71c76ba4f1b91d4
shadyendless <dalindeck@gmail.com>
2013-12-07 20:24:08 -0800

    Added basis for flash deals.

--- UniverCity/HTML_UI/VirtualMall/js/virtualmall.js
+++ UniverCity/HTML_UI/VirtualMall/js/virtualmall.js
@@ -4,7 +4,34 @@ $(function () {
         engine.call('GetBusinessSubCat', this.getAttribute('catid'));
     });
 
+    var URL = "http://app2.univercity3d.com/univercity/GetFlashDeals";
+    //Parameter passing breaks iOS so comment out hen building to iOS
+    $.ajax({url: URL, success: function(flashDealData){
+        console.log(flashDealData);
+        AddFlashDeals(flashDealData);
+    }});
 });
+
+function AddFlashDeals(flashDealData) {
+    if (!flashDealData.s && flashDealData.deals.length <= 0) return;
+    for(var i = 0; i < flashDealData.deals.length; ++i) {
+        $('.flash-carousel').append('<div class="flash-deal">' +
+            '<h1><i class="fa fa-bolt fa-lg"></i> FLASH DEAL <i class="fa fa-bolt fa-lg"></i></h1>' +
+            '<div class="flash-deal-inner">' +
+            '<p class="line-1">' + flashDealData.deals[i].title1 + '</p>' +
+            '<p class="line-2">' + flashDealData.deals[i].title2 + '</p>' +
+            '<p class="line-3">' + flashDealData.deals[i].title3 + '</p>' +
+        '</div>' +
+        '</div>');
+    }
+
+    $('.flash-carousel').owlCarousel({
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3],
+        itemsTablet: [768, 3],
+        pagination: true
+    });
+}
 
 $(function () {
     var container = document.getElementById('st-container');
