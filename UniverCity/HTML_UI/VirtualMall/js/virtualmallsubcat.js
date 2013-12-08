dade0e748e414298cb5b59264d8b4148e575d1c0
shadyendless <dalindeck@gmail.com>
2013-12-07 19:04:18 -0800

    Virtual Mall Subcategory fixes.

--- UniverCity/HTML_UI/VirtualMall/js/virtualmallsubcat.js
+++ UniverCity/HTML_UI/VirtualMall/js/virtualmallsubcat.js
@@ -5,19 +5,31 @@ $(document).ready(function () {
 
 engine.on('CreateCategory', function (cat) {
     console.log("Here");
-    document.getElementById('container').innerHTML += '<div class="category"><h4>' + cat + '</h4></div>';
+    document.getElementById('container').innerHTML += '<div class="category">' +
+        '<h4>' + cat + '</h4>' +
+        '<div class="owl-carousel"></div>'
+        '</div>';
 })
 
 engine.on('PopulateCategory', function (name, desc, id, image, index, hasAd) {
-
+    console.log('HERE');
     if(hasAd === true)
-        document.getElementsByClassName('category')[index].innerHTML += '<div class="business"><header>' + name + '</header><figure><img src="data:image/png;base64,' + image
-            + '" /><figcaption>' + desc + '</figcaption></figure><button type="button" busid="' + id + '"class="btn btn-see-more">See More</button><a busid="'+ id
-            +'">Contact Info</a></div>';
+        document.getElementsByClassName('owl-carousel')[index].innerHTML += '<div class="business">' +
+            '<header>' + name + '</header>' +
+            '<figure>' +
+            '<img src="data:image/png;base64,' + image + '" />' +
+            '<p>' + desc + '</p>' +
+            '</figure>' +
+            '<button type="button" busid="' + id + '"class="btn btn-see-more">See More</button>' +
+            '<a busid="'+ id +'">Contact Info</a></div>';
     else
-        document.getElementsByClassName('category')[index].innerHTML += '<div class="business"><header>' + name + '</header><figure><img src="data:image/png;base64,' + image
-            + '" /><figcaption>' + desc + '</figcaption></figure><a busid="'+id
-            +'">Contact Info</a></div>';
+        document.getElementsByClassName('owl-carousel')[index].innerHTML += '<div class="business">' +
+            '<header>' + name + '</header>' +
+            '<figure>' +
+            '<img src="data:image/png;base64,' + image + '" />' +
+            '<p>' + desc + '</p>' +
+            '</figure>' +
+            '<a busid="'+id +'">Contact Info</a></div>';
 })
 
 engine.on('CreateEmptyCategory', function(index) {
@@ -27,6 +39,14 @@ engine.on('CreateEmptyCategory', function(index) {
 })
 
 engine.on('AttachEventToBusinesses', function () {
+    $('.owl-carousel').owlCarousel({
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3],
+        itemsTablet: [768, 3],
+        pagination: true
+    });
+
+
     $(".business > button").click(function () {
         console.log(this.getAttribute('busid'));
         engine.call('SetBusinessID', this.getAttribute('busid'));
