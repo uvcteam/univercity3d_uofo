0024a2ef39ea4cd3620418d704c03e9bde7ef7f9
Alex Ford <alex.ford1337@gmail.com>
2014-01-09 13:37:09 -0800

    Fix for issue T55. Flash deals now correctly show multiple parts.

--- UniverCity/HTMLUI/VirtualMall/js/flashdeal.js
+++ UniverCity/HTMLUI/VirtualMall/js/flashdeal.js
@@ -11,44 +11,73 @@ $(document).ready(function () {
 });
 
 engine.on('LoadFlashPlayer', function(flashdeal, URL){
-    console.log(flashdeal);
-    URL += "admedia?id=";
-    var data = jQuery.parseJSON(flashdeal);
-    console.log(data);
-    for(var i = 0; i < data.parts.length; ++i) {
-    	switch(data.parts[i].type)
-    	{
-    		case "image":
-    			$('#container').append('<img src="'+ URL + data.parts[i].id +'"></img>');
-    		break;
-    		
-    		case "video":
-    			$('#container').append('<div class="adpage vzaar_media_player" data-title="" data-details="" data-narration="">'
-                    + '<object data="http://view.vzaar.com/1417694/flashplayer" height="324" id="vzvid'+ i +'" type="application/x-shockwave-flash" width="576">' +
-                    '<param name="wmode" value="transparent" /><param name="allowFullScreen" value="true" />' +
-                    '<param name="movie" value="http://view.vzaar.com/1417694/flashplayer" />' +
-                    '<param name="allowScriptAccess" value="always" />' +
-                    '<param name="autoStart" value="true" />' +
-                    '<param name="flashvars" value="border=none&amp;showplaybutton=rollover" />' +
-                    '<video data-played="false" preload="metadata" controls height="324" id="htmlvid" onclick="this.play();"'+
-                    'poster="'+ URL + data.parts[i].id +'&thumbnail=1' +'" preload="none" src="http://www.univercity3d.com/univercity/admedia?id=' + data.parts[i].id+ '" width="576" ></video></object>'
-                    +'</div>');
-    		break;
-    		
-    		case "text":
-    			$('#container').append(data.parts[i].text);
-    		break;
-    	}
+    var mediaURL = URL + "admedia?id=";
+    var adpage = jQuery.parseJSON(flashdeal);
+    console.log(adpage);
+
+    var style = "";
+    var  pageItem = '<div class="page"><table class="adpage" data-title="" data-details="" data-narration="' + adpage.narrative + '">';
+    var partType = "one";
+
+    switch (adpage.parts.length)
+    {
+        case 1 :
+            partType = "one";
+            break;
+        case  2:
+            partType = "two";
+            break;
+        case 3:
+            partType = "three";
+            break;
+        case 4:
+            partType = "four";
+            break;
     }
+    pageItem += '<tr>';
+
+
+    for (var i =0; i < adpage.parts.length; ++i){
+
+        if( adpage.parts.length == 4 && i == 2)
+        {
+            pageItem += '</tr><tr>';
+        }
+
+        pageItem += '<td class="'+ partType +'">';
+
+        switch(adpage.parts[i].type)
+        {
+            case "image":
+                pageItem += '<img src="' + mediaURL + adpage.parts[i].id + '"/>';
+                break;
+            case "video":
+                pageItem += '<div class="vzaar_media_player">'
+                    +'<video draggable="true" data-played="false" preload="metadata" controls id="" onclick="this.play();" poster="'+ mediaURL + adpage.parts[i].id +'&thumbnail=1'
+                    +'"preload="none" src="' + mediaURL + adpage.parts[i].id + '"></video>'
+                    +'</div>';
+                break;
+            case "text":
+                pageItem += '<div>'
+                    + adpage.parts[i].text + '</div>';
+        }
+
+        pageItem += '</td>';
+    }
+    pageItem += '</tr>';
+
+    pageItem += '</table>';
     
-    if (data.expert)
-    	SetNarrator(URL + data.expert.id);
+    if (adpage.expert)
+    	SetNarrator(URL + adpage.expert.id);
     else
     	$('#narrator-img').attr('style', 'display:none');
     	
-    if (data.narrative === "")
+    if (adpage.narrative === "")
         $('#narrator-bubble').hide();
-    $('#narrator-text').text(data.narrative);
+    $('#narrator-text').text(adpage.narrative);
+
+    $('#container').append(pageItem);
     
 })
 
