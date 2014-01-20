2b7fef642fd39aaf41905e93453f7a2781a57df2
Alex Ford <alex.ford1337@gmail.com>
2014-01-19 22:50:28 -0800

    Removed old code that was causing the Android build to crash when loading a scene from the VM.

--- UniverCity/HTMLUI/VirtualMall/js/virtualmall.js
+++ UniverCity/HTMLUI/VirtualMall/js/virtualmall.js
@@ -54,21 +54,3 @@ function AddFlashDeals(flashDealData) {
     });
 }
 
-$(function () {
-    var container = document.getElementById('st-container');
-    $('#menu_btn').click(function () {
-        container.className = 'st-container';
-        classie.add(container, 'st-effect-1');
-        setTimeout(function () {
-            classie.add(container, 'st-menu-open');
-        }, 25);
-    });
-    $('.st-container li').click(function (event) {
-        event.stopPropagation();
-        if (this.getAttribute('destination') != 'internal')
-            GoToDestination(this.getAttribute('destination'));
-    });
-    $('.st-container').click(function (event) {
-        classie.remove(document.getElementById('st-container'), 'st-menu-open');
-    });
-});
