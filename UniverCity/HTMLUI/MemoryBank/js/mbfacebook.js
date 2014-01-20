c991709361408b34ac620b6d619ecfdb1fef5fd4
shadyendless <dalindeck@gmail.com>
2014-01-20 13:03:45 -0800

    CSS fixes for various areas.

--- UniverCity/HTMLUI/MemoryBank/js/mbfacebook.js
+++ UniverCity/HTMLUI/MemoryBank/js/mbfacebook.js
@@ -7,7 +7,7 @@ function FBSignIn() {
 }
 
 function FBSignOut() {
-    $('#fb_link').html('<a href="#" onclick="FBSignIn()">Connect to Facebook!</a>');
+    $('#fb_link').html('<button class="btn btn-facebook" onclick="FBSignIn()"><i class="fa fa-facebook"></i> | Connect with Facebook</button>');
     engine.call("SignOutOfFacebook");
     $('#profile-pic').attr('src', 'images/profile.svg');
     $('#user-name').html('Loading Name');
@@ -17,7 +17,7 @@ function FBSignOut() {
 
 engine.on('FacebookInfoMB', function(result) {
     var fbo = jQuery.parseJSON(result);
-      $('#fb_link').html('<a href="#" onclick="FBSignOut()">Disconnect from Facebook!</a>');
+      $('#fb_link').html('<button class="btn btn-facebook" onclick="FBSignOut()"><i class="fa fa-facebook"></i> | Disconnect from Facebook</button>');
     $('#user-name').html(fbo.name);
     $('#profile-pic').attr('src', fbo.picture.data.url);
     $('#user-quotes').html(fbo.quotes);
