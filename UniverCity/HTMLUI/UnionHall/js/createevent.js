e1099b405764f315b53675ce3ec9908041ea0686
Jacob Foster <dalindeck@gmail.com>
2014-05-23 21:10:40 -0700

    Started work on dynamically loading the categories while creating events. Added the server_url JavaScript file that we can switch to production or development URLs. Just include this JS file before anything else and you can access the proper URL with server_url

--- UniverCity/HTMLUI/UnionHall/js/createevent.js
+++ UniverCity/HTMLUI/UnionHall/js/createevent.js
@@ -6,6 +6,16 @@
 // cache selects for use later
 var selects = $('.chosen-select');
 
+$(document).ready(function() {
+    window.interests = [];
+
+    $.get(server_url + "/ListSocialInterests", function(data) {
+        $(data).each(function() {
+            interests.push(this);
+        });
+    });
+});
+
 
 // whenever the selection changes, either disable or enable the 
 // option in the other selects
@@ -36,21 +46,38 @@ function OnEventCreate() {
     $('#myModal').modal('show');
 }
 
-function SubmitEvent() {
-    var values = $(":input").serializeArray();
-    var inputs = new Array();
-    inputs[0]  = values[0]['value'];
-    inputs[1]  = values[1]['value'];
-    inputs[2]  = values[6]['value'];
-    inputs[3]  = values[4]['value'];
-    inputs[4]  = values[2]['value'];
-    inputs[5]  = values[7]['value'];
-    inputs[6]  = values[5]['value'];
-    inputs[7]  = values[9]['value'];
-    inputs[8]  = values[3]['value'];
-    inputs[9] = values[8]['value'];
+function SubmitEvent() 
+{
+    window.event_name = $("#event_name");
+    window.event_date = $("#event_date");
+    window.event_time = $("#event_time");
+    window.event_what = $("#event_what");
+    window.event_who = $("#event_who");
+    window.event_min = $("#event_min");
+    window.event_max = $("#event_max");
+    window.event_where = $("#event_where");
+    window.event_phone = $("#event_phone");
+    window.categories = [];
+
+    $('.chosen-select').find("option").each(function() {
+        if (this.selected && this.value != 'Choose a category')
+            categories.push(this.value);
+    });
+
+    // var values = $(":input").serializeArray();
+    // var inputs = new Array();
+    // inputs[0]  = values[0]['value'];
+    // inputs[1]  = values[1]['value'];
+    // inputs[2]  = values[6]['value'];
+    // inputs[3]  = values[4]['value'];
+    // inputs[4]  = values[2]['value'];
+    // inputs[5]  = values[7]['value'];
+    // inputs[6]  = values[5]['value'];
+    // inputs[7]  = values[9]['value'];
+    // inputs[8]  = values[3]['value'];
+    // inputs[9] = values[8]['value'];
     
-    engine.call('CreateEvent', inputs);
+    // engine.call('CreateEvent', inputs);
 }
 
 // Unity3D Functions.
---
+++ UniverCity/HTMLUI/libs/server_url.js
@@ -0,0 +1,2 @@
+var server_url = 'http://app2.univercity3d.com/univercity/';
+//var server_url = 'http://app.univercity3d.com/univercity/';
